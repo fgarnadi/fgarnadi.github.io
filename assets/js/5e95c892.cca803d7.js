9aef475ea83f7833ae0a3d71ab0702f1c35c2a24
fgarnadi <fgarnadi@users.noreply.github.com>
2025-08-13 13:08:13 +0000

    deploy: c6b13e03ec5f221b41a35a6b6051b6cdbe7cd072

--- assets/js/5e95c892.cca803d7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkatelier=self.webpackChunkatelier||[]).push([[647],{5436:(e,r,s)=>{s.r(r),s.d(r,{default:()=>i});s(758);var a=s(3526),t=s(5484),u=s(6619),c=s(389),l=s(5406),n=s(6070);function i(e){return(0,n.jsx)(t.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,n.jsx)(l.A,{children:(0,c.v)(e.route.routes)})})}}}]);
